5ce4780b10f04cef224a53b208700ac97d5f6a4d
Abed <abedhamami42@gmail.com>
2022-03-01 15:21:26 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,62 @@
+//import { Chess } from "chess.js";
+
+const { Chess } = require("chess.js");
+
+let btn = document.querySelector(".press");
+btn.addEventListener("click", flip);
+
+function move2d4() {
+  // board.move("d2-d6");
+  board.move("wQ-e8");
+}
+
+function flip() {
+  board.flip();
+  console.log(board.orientation());
+}
+
+function whenDragStartsLogEverything(source, piece, position, orientation) {
+  console.log("Drag started:");
+  console.log("Source: " + source);
+  console.log("Piece: " + piece);
+  // console.log("Position: " + Chessboard/.objToFen(position));
+  console.log("Orientation: " + orientation);
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+}
+
+function alsoLogEverythingOnDrop(
+  source,
+  target,
+  piece,
+  newPos,
+  oldPos,
+  orientation
+) {
+  console.log("Source: " + source);
+  console.log("Target: " + target);
+  console.log("Piece: " + piece);
+  // console.log("New position: " + Chessboard.objToFen(newPos));
+  // console.log("Old position: " + Chessboard.objToFen(oldPos));
+  console.log("Orientation: " + orientation);
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+}
+
+const config = {
+  draggable: true,
+  pieceTheme: "https://chessboardjs.com/img/chesspieces/wikipedia/{piece}.png",
+  position: "start",
+  onDragStart: whenDragStartsLogEverything,
+  onDrop: alsoLogEverythingOnDrop
+};
+
+const board = Chessboard("daBoard", config);
+const chess = new Chess();
+
+while (!chess.game_over()) {
+  const moves = chess.moves();
+  console.log(JSON.stringify(moves));
+  const move = moves[Math.floor(Math.random() * moves.length)];
+  chess.move(move);
+}
+console.log(chess.pgn());
+// button. when you press it. you move the d2 pawn to d4;
