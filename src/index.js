6411c8d84778a8edada11302c6e844b27fbcb115
avaeazadi2021 <avaeazadi2@gmail.com>
2022-03-04 19:30:34 +0200

    Added stuff

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 //import { Chess } from "chess.js";
 
-const { Chess } = require("chess.js");
+//const { Chess } = require("chess.js");
 
-let btn = document.querySelector(".press");
+let btn = document.querySelector(".press"); 
 btn.addEventListener("click", flip);
 
-function move2d4() {
+function move2d4() {         
   // board.move("d2-d6");
   board.move("wQ-e8");
 }
@@ -60,3 +60,140 @@ while (!chess.game_over()) {
 }
 console.log(chess.pgn());
 // button. when you press it. you move the d2 pawn to d4;
+
+
+
+
+
+
+
+
+
+// The New Codes Starts//
+var Stopwatch = function(elem, options) {
+
+  var timer = createTimer(),
+    startButton = createButton("start", start),
+    stopButton = createButton("stop", stop),
+    resetButton = createButton("reset", reset),
+    offset,
+    clock,
+    interval;
+
+  // default options
+  options = options || {};
+  options.delay = options.delay || 1;
+
+  // append elements     
+  elem.appendChild(timer);
+  elem.appendChild(startButton);
+  elem.appendChild(stopButton);
+  elem.appendChild(resetButton);
+
+  // initialize
+  reset();
+
+  // private functions
+  function createTimer() {
+    return document.createElement("span");
+  }
+
+  function createButton(action, handler) {
+    var a = document.createElement("a");
+    a.href = "#" + action;
+    a.innerHTML = action;
+    a.addEventListener("click", function(event) {
+      handler();
+      event.preventDefault();
+    });
+    return a;
+  }
+
+  function start() {
+    if (!interval) {
+      offset = Date.now();
+      interval = setInterval(update, options.delay);
+    }
+  }
+
+  function stop() {
+    if (interval) {
+      clearInterval(interval);
+      interval = null;
+    }
+  }
+
+  function reset() {
+    clock = 0;
+    render(0);
+  }
+
+  function update() {
+    clock += delta();
+    render();
+  }
+
+  function render() {
+    timer.innerHTML = clock / 1000;
+  }
+
+  function delta() {
+    var now = Date.now(),
+      d = now - offset;
+      offset = now;
+      return d;
+  }
+
+  // public API
+  this.start = start;
+  this.stop = stop;
+  this.reset = reset;
+};
+
+// basic examples
+var elems = document.getElementsByClassName("basic");
+
+for (var i = 0, len = elems.length; i < len; i++) {
+  new Stopwatch(elems[i]);
+}
+
+//..........................DATE TIME
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  let ms=today.getMilliseconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s ;
+  setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+
+
+function brown(){ document.body.style.background = "white"; }
+function darkBrown(){ document.body.style.background = "rgb(104, 43, 19)"; }
+
+
+
+
+/* movenemt <codes*/
+//document.body.style.backgroundColor = "red";//     change the body color //
+
+
+
+
+
+
+/* box in js*/
+el=document.getElementById("box1");
+width=window.innerWidth +50;
+height=window.innerHeight+50;
+el.style.left=width*Math.random();
+el.style.top=height*Math.random();
